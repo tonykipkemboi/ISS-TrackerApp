0ca2668dac1188881e71f4dabe978b9944fef10d
tonyk <tonykip@seas.upenn.edu>
2021-02-14 02:05:39 -0500

    2 nd edit

--- ISS_App.py
+++ ISS_App.py
@@ -1,37 +1,154 @@
 # -*- coding: utf-8 -*-
 """
-This app tracks the current location of the ISS
-and current astornauts on board the ISS
+@Author: tonykip
+@Sources: nasa, wikipedia, and https://projects.raspberrypi.org/en/projects/where-is-the-space-station
+
+This app tracks the current location of the ISS and current Astronauts on board the ISS
+
 ISS = International Space Station
 
 """
 
+# --------------------------------------------------------------------------------------------------------------------#
 # Import dependencies
 import json
-import turtle 
+import turtle
 import urllib.request
 import time
 import webbrowser
+
 import geocoder
 
-# URL for the API to locate ISS
+# --------------------------------------------------------------------------------------------------------------------#
+# Global variables
+# URL for the API's
+url_astro = 'http://api.open-notify.org/astros.json'
 url_loc = 'http://api.open-notify.org/iss-now.json'
 
-# URL for the API to show astronauts onboard the ISS
-url_astro = 'http://api.open-notify.org/astros.json'
-response = urllib.request.urlopen(url_astro)
-result = json.loads(response.read())
-file = open("iss.txt", "w")
-file.write("There are " + str(result["number"]) + 
-           " Astronauts currently on the International Space Station!")
+# turtle instance variables
+screen = turtle.Screen()
+iss = turtle.Turtle()
 
-people = result['people']
-for person in people:
-    file.write(person['name'] + " - on board the ISS" + "\n")
 
-# print longitude and latitude
-g = geocoder.ip("me")
-file.write("\n Your current latitude / longitude is: " + str(g.latlng))
-file.close()
+# --------------------------------------------------------------------------------------------------------------------#
+# Return a list of astronauts currently aboard the ISS
+def astronauts_on_board_iss():
+    # URL for the API to show astronauts onboard the ISS
+    response_1 = urllib.request.urlopen(url_astro)
+    result_1 = json.loads(response_1.read())
 
-webbrowser.open("iss.txt")
+    file = open("iss.txt", "w")
+    file.write("Currently, there are " + str(result_1["number"]) +
+               " Astronauts aboard the ISS.\n" + "\nThey are: " + "\n")
+
+    people = result_1['people']
+
+    # loop over list of astros and write to file
+    for person in people:
+        file.write("\n - " + person['name'] + "\n")
+
+    # get my longitude and latitude
+    g = geocoder.ipinfo("me")
+    file.write("\nYour current latitude / longitude is: " + str(g.latlng))
+    file.write("\nYour current location is: " + g.city + ", " + g.state + ", " + g.country)
+
+    # close file
+    file.close()
+
+    # open file
+    return webbrowser.open("iss.txt")
+
+
+# --------------------------------------------------------------------------------------------------------------------#
+# Setup the world map and the ISS on turtle module
+def setup_world_map_and_iss():
+    screen.setup(1200, 635)
+    screen.setworldcoordinates(-180, -90, 180, 90)
+
+    # load the map and iss
+    screen.bgpic("world.gif")
+    screen.register_shape("iss.gif")
+    iss.shape("iss.gif")
+    iss.setheading(45)
+    iss.penup()
+
+
+# --------------------------------------------------------------------------------------------------------------------#
+# method to find current location of the ISS
+def locate_iss():
+    # URL for the API to show current location of ISS
+    response = urllib.request.urlopen(url_loc)
+    result = json.loads(response.read())
+
+    # extract ISS location (latitude and longitude)
+    location = result["iss_position"]
+    lat = location["latitude"]
+    lon = location["longitude"]
+
+    # output lon and lat on terminal
+    lat = float(lat)
+    lon = float(lon)
+    print("\nLatitude: " + str(lat))
+    print("\nLongitude: " + str(lon))
+
+
+# --------------------------------------------------------------------------------------------------------------------#
+# draws path taken by ISS -> in 'real-time'
+def draw_iss_route():
+    # URL for the API to show current location of ISS
+    response = urllib.request.urlopen(url_loc)
+    result = json.loads(response.read())
+
+    # pen and color to draw path
+    iss.pensize(3)
+    iss.pencolor("red")
+    iss.penup()
+
+    # extract ISS location (latitude and longitude)
+    location = result["iss_position"]
+    lat = location["latitude"]
+    lon = location["longitude"]
+
+    # output longitude and latitude on terminal
+    lat = float(lat)
+    lon = float(lon)
+    iss.setposition(float(lon), float(lat))
+    iss.pd()
+
+    # update ISS location on the map
+    iss.goto(lon, lat)
+
+
+# --------------------------------------------------------------------------------------------------------------------#
+# function to execute the program
+def main():
+    print("===============================================================================================")
+    print("This is an app to track the ISS and the names of those currently on board the station! \nEnjoy!")
+    print("===============================================================================================")
+    print(" ")
+
+    # outputs a file with names of astronauts onboard the ISS
+    astronauts_on_board_iss()
+
+    while True:
+        # loads the map and iss tracker
+        setup_world_map_and_iss()
+
+        # shows current location of the ISS
+        locate_iss()
+
+        # draws the path taken by the ISS over time
+        draw_iss_route()
+
+        # refresh every 5 seconds
+        time.sleep(5)
+
+        # exit program if and only if app screen is clicked
+        screen.exitonclick()
+
+
+# --------------------------------------------------------------------------------------------------------------------#
+# top-level main scope script
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # ISS Tracker App
-App that tracks the current location of the International Space Station (ISS)  and Astronauts on board.
+
+This is a simple app that tracks the current location of the ISS and Astronauts on board the ISS.
+
+(ISS == _International Space Station_)
+
+## Description
+
+I am a space enthusiast and this project is a fun way for me to keep polishing my programming skills while engaging with 
+a topic I like; you know, space 'n stuff.
+
+I got the inspiration for this project from [here](https://projects.raspberrypi.org/en/projects/where-is-the-space-station) 
+and I will be continuously adding my own twist as time goes.
+
+Some quick Facts about the ISS [Sources:WikiPedia & NASA]:
+
+1. Launched: 20 November 1998
+
+2. Mass: 419,725 kg (925,335 lbs), Length: 73.0 m (239.4 ft), Width: 109.0 m (357.5 ft)
+
+3. Perigee Altitude: 418 km (259.7 miles)
+
+4. Apogee Altitude: 422 km (262.2 miles)
+
+5. Orbital speed: 7.66 km/s (17,100 mph)
+
+6. Orbital period: 92.68 minutes, Orbits per day: 15.54, Orbital decay: 2 km/month
+
+7. Fully crewed: 7 Astronauts
+
+
+For more information, visit: https://www.nasa.gov/mission_pages/station and https://en.wikipedia.org/wiki/International_Space_Station
+
+## Installation / Usage
+
+Download or clone the repository.
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install the dependencies if not installed.
+
+```bash
+pip install requirements.txt
+```
+Run the program 
+
+```bash
+python ISS_App.py
+```
+
+## Contributing and Support
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change or add.
+
+Advice/critique is very welcomed as well.
+
+Thank you for contributing!
+
+## Visual
+
+This is a screenshot visual of the application running over n period of time to show the path taken by the ISS.
+
+![App Screenshot](https://github.com/tonykipkemboi/ISS_Tracker_Project/blob/static_visual.JPG?raw=true)
+
+
